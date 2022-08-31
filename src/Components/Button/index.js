db1020d7b7e6c82c8d245027513e24d82971fa99
MageLink <mage.link.zhezard@gmail.com>
2022-08-30 23:26:51 -0400

    backup

--- src/Components/Button/index.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ status, handleLogin, logInStatus }) => {
-  return (
-    <button className="header-button" onClick={() => handleLogin(logInStatus)}>
-      {status}
-    </button>
-  );
-};
-
-export default Button;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,18 +2,13 @@ import GeneralContext from "../../Context/GeneralContext";
 // Context
 import { useContext } from "react";
 // Components
-import Button from "../Button";
 
 const Header = () => {
   const { logIn, handleLogin, handleLogOut } = useContext(GeneralContext);
   return (
     <div className="header-general-container">
-      <span>Logo</span>
-      {!logIn ? (
-        <Button status={"Log-in"} handleLogin={handleLogin} logInStatus={!logIn} />
-      ) : (
-        <Button status={"Log-out"} handleLogin={handleLogOut} logInStatus={!logIn} />
-      )}
+      <h3>Buscador de colaboradores</h3>
+      <input className="header-input" placeholder="Busca un colaborador" />
     </div>
   );
 };
