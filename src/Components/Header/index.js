0ba03831ab14be1732e9f63e14caceedb0a31ee6
MageLink <mage.link.zhezard@gmail.com>
2022-09-01 20:20:53 -0400

    Ejercicios

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,10 +4,11 @@ import { useContext } from "react";
 // Components
 
 const Header = () => {
-  const { logIn, handleLogin, handleLogOut } = useContext(GeneralContext);
+  const { logIn, handleLogin, handleLogOut, ejercicio } = useContext(GeneralContext);
   return (
     <div className="header-general-container">
       <h3>Buscador de colaboradores</h3>
+      <button onClick={ejercicio}>Ejercicios</button>
       <input className="header-input" placeholder="Busca un colaborador" />
     </div>
   );
--- src/Context/GeneralContext/Provider.js
+++ src/Context/GeneralContext/Provider.js
@@ -2,6 +2,33 @@ import { useState } from "react";
 // Context
 import GeneralContext from "./";
 
+const passengers = [
+  {
+    id: 1,
+    passengerName: "Freddie Mercury",
+    isVegetarianOrVegan: false,
+    connectedFlights: 2,
+  },
+  {
+    id: 2,
+    passengerName: "Amy Winehouse",
+    isVegetarianOrVegan: true,
+    connectedFlights: 4,
+  },
+  {
+    id: 3,
+    passengerName: "Kurt Cobain",
+    isVegetarianOrVegan: true,
+    connectedFlights: 3,
+  },
+  {
+    id: 3,
+    passengerName: "Michael Jackson",
+    isVegetarianOrVegan: true,
+    connectedFlights: 1,
+  },
+];
+
 const GeneralContextProvider = ({ children }) => {
   const [logIn, setLogIn] = useState(false);
 
@@ -11,9 +38,12 @@ const GeneralContextProvider = ({ children }) => {
   const handleLogOut = (handler) => {
     setLogIn(handler);
   };
+  const ejercicio = () => {
+    console.log(passengers.map((info) => info.passengerName));
+  };
 
   return (
-    <GeneralContext.Provider value={{ logIn, setLogIn, handleLogin, handleLogOut }}>
+    <GeneralContext.Provider value={{ logIn, setLogIn, handleLogin, handleLogOut, ejercicio }}>
       {children}
     </GeneralContext.Provider>
   );
